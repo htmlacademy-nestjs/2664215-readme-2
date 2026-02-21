2f7d438987e231c93370d6308138de0fc6ddc263
Timur <Timur>
2026-02-21 14:01:17 +0400

    2.2. Add entities interfaces

--- project/libs/shared/types/src/index.ts
+++ project/libs/shared/types/src/index.ts
@@ -1,1 +1,11 @@
-export * from './lib/types.js';
+export { PostType } from './lib/post-type.enum';
+export type { AuthUser } from './lib/auth-user.interface';
+export type {
+  Post,
+  LinkPost,
+  QuotePost,
+  PhotoPost,
+  TextPost,
+  VideoPost,
+} from './lib/post.interface';
+export type { User } from './lib/user.interface';
---
+++ project/libs/shared/types/src/lib/post.interface.ts
@@ -0,0 +1,37 @@
+import { PostType } from './post-type.enum';
+
+export interface Post {
+  id?: string;
+  type: PostType;
+  tags?: string[];
+}
+
+export interface LinkPost extends Post {
+  type: PostType.Link;
+  linkUrl: string;
+  description: string;
+}
+
+export interface QuotePost extends Post {
+  type: PostType.Quote;
+  quote: string;
+  quoteAuthor: string;
+}
+
+export interface TextPost extends Post {
+  type: PostType.Text;
+  title: string;
+  announce: string;
+  text: string;
+}
+
+export interface PhotoPost extends Post {
+  type: PostType.Photo;
+  photoUrl: string;
+}
+
+export interface VideoPost extends Post {
+  type: PostType.Video;
+  title: string;
+  videoUrl: string;
+}
