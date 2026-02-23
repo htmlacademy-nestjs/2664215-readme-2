6809496d34d41951adc46ef9ca8589dc0e436406
Timur <Timur>
2026-02-23 11:54:51 +0400

    2.14. Inject BlogUserRepository to AuthenticationService

--- project/apps/account/src/authentication/authentication.module.ts
+++ project/apps/account/src/authentication/authentication.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AuthenticationController } from './authentication.controller';
 import { AuthenticationService } from './authentication.service';
+import { BlogUserRepository } from "./blog-user.repository";
 
 @Module({
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [AuthenticationService, BlogUserRepository],
 })
 export class AuthenticationModule {}
--- project/apps/account/src/authentication/blog-user.repository.ts
+++ project/apps/account/src/authentication/blog-user.repository.ts
@@ -1,5 +1,8 @@
+import { Injectable } from "@nestjs/common";
+
 import { BaseMemoryRepository } from '@project/core';
 
 import { BlogUserEntity } from './blog-user.entity';
 
+@Injectable()
 export class BlogUserRepository extends BaseMemoryRepository<BlogUserEntity> {}
