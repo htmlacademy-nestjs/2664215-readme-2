116277bad43e378e6f6e65f1f955108fb40c7e45
Timur <Timur>
2026-02-24 21:08:18 +0400

    2.24. Add spec

--- project/apps/account/src/app/authentication/authentication.controller.ts
+++ project/apps/account/src/app/authentication/authentication.controller.ts
@@ -1,28 +1,49 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Post } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { fillRdo } from '@project/helpers';
 
 import { AuthenticationService } from './authentication.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoggedUserRdo } from './rdo/logged-user.rdo';
 import { LoginUserDto } from './dto/login-user.dto';
 import { UserRdo } from './rdo/user.rdo';
 
+@ApiTags('authentication')
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authorizationService: AuthenticationService) {}
 
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The new user has been successfully created.',
+  })
   @Post('register')
   public async register(@Body() dto: CreateUserDto) {
     const user = await this.authorizationService.register(dto);
     return fillRdo(UserRdo, user.convertToObject());
   }
 
+  @ApiResponse({
+    type: LoggedUserRdo,
+    status: HttpStatus.OK,
+    description: 'User has been successfully logged.',
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Password or Login is wrong.',
+  })
   @Post('login')
   public async login(@Body() dto: LoginUserDto) {
     const user = await this.authorizationService.verify(dto);
     return fillRdo(UserRdo, user.convertToObject());
   }
 
+  @ApiResponse({
+    type: UserRdo,
+    status: HttpStatus.OK,
+    description: 'User found',
+  })
   @Get(':id')
   public async show(@Param('id') id: string) {
     const user = await this.authorizationService.getUser(id);
