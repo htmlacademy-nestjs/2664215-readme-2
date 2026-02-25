3a08cc1e3d0caa92cf3fd3e3b828380b5a66379d
Timur <Timur>
2026-02-25 10:20:39 +0400

    2.25. Add PostModule

--- project/apps/blog/src/app/app.module.ts
+++ project/apps/blog/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { PostModule } from './post/post.module';
+
 @Module({
-  imports: [],
+  imports: [PostModule],
   controllers: [],
   providers: [],
 })
